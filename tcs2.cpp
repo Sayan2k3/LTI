54994ababb49615c54869d0458cb545304e8d0e5
Sayan Paul <121636215+Sayan2k3@users.noreply.github.com>
2025-11-02 01:52:26 +0530

    Add files via upload

---
+++ tcs2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int count=0;
+    int i=1;
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i+1]){
+            count++;
+        }
+        
+    }
+    cout<<count;
+    return 0;
+    
+}
+
+
+
+
