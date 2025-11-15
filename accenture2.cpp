57eb642ecffd569112580ab58c0bbc848ea4d06b
Sayan Paul <121636215+Sayan2k3@users.noreply.github.com>
2025-11-16 00:51:22 +0530

    Add files via upload

---
+++ accenture2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    if(s=="rock") cout<<"paper";
+    else if(s=="scissors") cout<<"rock";
+    else cout<<"scissor";
+    
+}
